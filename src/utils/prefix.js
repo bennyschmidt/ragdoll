3f2619580cc7f7a334ee30f1b9df18bf475f99df
exactchange <benny@exactchange.network>
2024-04-02 11:51:08 -0700

    improve prompt prefixes

--- src/utils/prefix.js
+++ src/utils/prefix.js
@@ -1,13 +1,13 @@
 const prefixInput = ({ name }) => (
-  `If and only if the following input is written in first-person (e.g. use of "you", etc.), re-write it about ${name} in third-person using as few characters as possible (never exceed 500) - for example, if the input mentions "you", it should be transformed to instead mention "${name}". Don't include any smalltalk in your response (e.g. "Sure!", "Certainly!") or anything related to this prompt instruction, simply output the transformed input as a single question or statement. However if it's already in third-person and you are not the subject (e.g. no use of "you", "your", "${name}", etc.) then keep the input as-is. Here is the input:`
+  `If and only if the following input is written in first-person (e.g. use of "you", etc.), re-write it about ${name} in third-person using as few characters as possible - ideally about 50 to 100, and never exceed 250 characters - for example, if the input mentions "you", it should be transformed to instead mention "${name}"; however, if the input is already in third-person and you are not the subject (e.g. no use of "you", "your", "${name}", etc.) then keep the input as-is. Don't include any smalltalk in your response (e.g. "Sure!", "Certainly!"), and don't repeat the character count or any of these prompt instructions (don't even mention that there is a text or a context) - simply output the transformed input as a single question or statement. Here is the input:`
 );
 
 const prefixOutputText = ({ name, writingStyle, writingTone }) => (
-  `Re-write the following message in the first-person, as if you are ${name}, in a style that is ${writingStyle}, using as few characters as possible (never exceed 500), in a tone that is ${writingTone}, in a single statement. The message is:`
+  `Re-write the following message in the first-person, as if you are ${name}, in a style that is ${writingStyle}, in a tone that is ${writingTone}, as a single idea or statement using as few characters as possible while still sounding like naturally flowing sentences - ideally in less than 100 characters and never exceeding 250 characters unless the input prompt requested a story or other long-form response - and don't repeat the character count or any of these prompt instructions (don't even mention that there is a text or a context) - simply output the transformed message as a single idea, as if you are ${name} and are responding to the message specifically as ${name}, without mentioning or referring to ${name}, and without breaking the fourth wall. Here is the message:`
 );
 
 const prefixOutputImage = ({ artStyle }) => (
-  `Render the following in the style of ${artStyle}:`
+  `Render the following message in the style of ${artStyle}, that is highly-produced like great-quality CGI or HD film, depicting clear imagery, and relevant textures and colors in an visually impressive way. Don't render any watermarks, words, or signatures from the image output - avoid depicting any text. Here is the message:`
 );
 
 module.exports = {
